0d8b8050633d986f41271bbf1173f3e515bbcf59
Stavros <snikoletopoulos@hotmail.com>
2022-02-27 00:28:11 +0200

    Load the fonts and MealsNavigation component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import MealsNavigator from "app/navigation/MealsNavigator";
+
+const fetchFonts = async () => {
+	Font.loadAsync({
+		"open-sans": require("./app/assets/fonts/OpenSans-Regular.ttf"),
+		"open-sans-bold": require("./app/assets/fonts/OpenSans-Bold.ttf"),
+	});
+};
+
+const App = () => {
+	const [fontLoaded, setFontLoaded] = useState(false);
+
+	if (!fontLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setFontLoaded(true)}
+				onError={error => console.log(error)}
+			/>
+		);
+	}
+
+	return <MealsNavigator />;
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ app/navigation/MealsNavigator.ts
@@ -0,0 +1,16 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
+
+const MealNavigator = createStackNavigator({
+	Categories: CategoriesScreen,
+	CategoryMeals: {
+		screen: CategoryMealsScreen,
+	},
+	MealDetail: MealDetailScreen,
+});
+
+export default createAppContainer(MealNavigator);
