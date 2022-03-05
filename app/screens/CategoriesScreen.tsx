5de85ed67c53e355be72aabfe82ec9e185af4a54
Stavros <snikoletopoulos@hotmail.com>
2022-03-05 10:40:32 +0200

    Create stack on the CategoriesScreen

--- app/screens/CategoriesScreen.tsx
+++ app/screens/CategoriesScreen.tsx
@@ -1,18 +1,70 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	ViewStyle,
+	FlatList,
+	ListRenderItem,
+	Button,
+	TouchableOpacity,
+} from "react-native";
 
-type Props = {};
+import Category from "models/category";
+import { NavigationScreenProp } from "react-navigation";
+import { CATEGORIES } from "../data/dummy-data";
+
+interface Props {
+	[key: string]: any;
+}
 
 const CategoriesScreen: React.FC<Props> = props => {
+	const renderGridItem: ListRenderItem<Category> = itemData => {
+		const handleCategorySelect = () => {
+			props.navigation.navigate({ routeName: "CategoryMeals" });
+		};
+
+		return (
+			<TouchableOpacity style={styles.gridItem} onPress={handleCategorySelect}>
+				<View>
+					<Text>{itemData.item.title}</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
 	return (
-		<View>
-			<Text>CategoriesScreen</Text>
-		</View>
+		<FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
 	);
+};
+
+CategoriesScreen.navigationOptions = {
+	headerTitle: "Meal Categories",
+	headerStyle: {
+		backgroundColor: "#f47b20",
+	},
+	headerTint: {
+		color: "#fff",
+	},
 };
 
 export default CategoriesScreen;
 
-interface Styles {}
+interface Styles {
+	screen: ViewStyle;
+	gridItem: ViewStyle;
+}
 
-const styles = StyleSheet.create<Styles>({});
+const styles = StyleSheet.create<Styles>({
+	screen: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+
+	gridItem: {
+		flex: 1,
+		margin: 15,
+		height: 150,
+	},
+});
