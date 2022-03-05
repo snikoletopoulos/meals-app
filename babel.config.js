c8130c36241347ecd1356988b5666b01f2cc1ab3
Stavros <snikoletopoulos@hotmail.com>
2022-03-05 10:34:05 +0200

    Configure path alias

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,23 @@
 // @ts-ignore
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					alias: {
+						components: "./app/components",
+						assets: "./app/assets",
+						navigation: "./app/navigation",
+						screens: "./app/screens",
+						data: "./app/data",
+						models: "./app/models",
+						constants: "./app/constants",
+					},
+				},
+			],
+		],
+	};
 };
