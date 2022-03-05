9a08deb62b373f1a9fd85ee801e58c88c25ecd77
Stavros <snikoletopoulos@hotmail.com>
2022-03-05 20:36:59 +0200

    Style the header navigation

--- app/screens/CategoryMealsScreen.tsx
+++ app/screens/CategoryMealsScreen.tsx
@@ -1,13 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 
+import { CATEGORIES } from "../data/dummy-data";
+
 type Props = {};
 
 const CategoryMealsScreen: React.FC<Props> = props => {
+	const categoryId = props.navigation.getParam("categoryId");
+
+	const selectedCategory = CATEGORIES.find(
+		category => category.id === categoryId
+	);
+
 	return (
 		<View>
-			<Text>CategoryMealsScreen</Text>
+			<Text>THe Category Meals Screen!</Text>
+			<Text>{selectedCategory?.title}</Text>
 		</View>
 	);
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+	const categoryId = navigationData.navigation.getParam("categoryId");
+
+	const selectedCategory = CATEGORIES.find(category => category.id === categoryId);
+
+	return {
+		headerTitle: selectedCategory?.title,
+	}
 };
 
 export default CategoryMealsScreen;
