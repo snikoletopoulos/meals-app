fdb89060f3e1ce67305dc35e5ef0428edadda02c
Stavros <snikoletopoulos@hotmail.com>
2022-03-05 10:38:21 +0200

    Remove unused import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
