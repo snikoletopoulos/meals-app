d9807c774bf0ae479db64902839f4c072f34d5fc
Stavros <snikoletopoulos@hotmail.com>
2022-03-12 19:59:08 +0200

    Create favorites screen with dummy data

--- app/screens/FavoritesScreen.tsx
+++ app/screens/FavoritesScreen.tsx
@@ -1,17 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useMemo } from "react";
+import {
+	NativeStackScreenProps,
+	NativeStackNavigationOptions,
+} from "@react-navigation/native-stack";
 
-type Props = {};
+import { MEALS } from "data/dummy-data";
+
+import { TabParamList } from "navigation/TabNavigator";
+import MealList from "components/MealList";
+
+type Props = NativeStackScreenProps<TabParamList, "Favorites">;
 
 const FavoritesScreen: React.FC<Props> = props => {
-	return (
-		<View>
-			<Text>FavoritesScreen</Text>
-		</View>
+	const handleMealSelect = (mealId: string) => {
+		props.navigation.navigate("Favorites", {
+			screen: "MealScreen",
+			params: { mealId },
+		});
+	};
+
+	const favoriteMeals = useMemo(
+		() => MEALS.filter(meal => ["m1", "m2"].includes(meal.id)),
+		[]
 	);
+
+	return <MealList meals={favoriteMeals} onSelectMeal={handleMealSelect} />;
+};
+
+export const screenOptions: NativeStackNavigationOptions = {
+	headerTitle: "Your Favorites",
 };
 
 export default FavoritesScreen;
-
-interface Styles {}
-
-const styles = StyleSheet.create<Styles>({});
