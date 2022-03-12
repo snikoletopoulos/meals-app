52f01df9288dd0420276f5499da092a104b7b993
Stavros <snikoletopoulos@hotmail.com>
2022-03-12 19:57:22 +0200

    Add basic styling to meal detail screen

--- app/screens/MealDetailScreen.tsx
+++ app/screens/MealDetailScreen.tsx
@@ -1,20 +1,55 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { Text, View, Button } from "react-native";
+import {
+	NativeStackNavigationOptions,
+	NativeStackScreenProps,
+} from "@react-navigation/native-stack";
 
-type Props = {};
+import { MealParamList } from "navigation/MealsNavigator";
+import { MEALS } from "data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "components/HeaderButton";
+
+type Props = NativeStackScreenProps<MealParamList, "MealDetail">;
 
 const MealDetailScreen: React.FC<Props> = props => {
+	const mealId = props.route.params.mealId;
+
+	const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
 	return (
 		<View>
 			<Text>MealDetailScreen</Text>
-			<Button title="Go back" onPress={()=> {
-				props.navigation.popToTop();
-			}} />
+			<Text>{selectedMeal?.title}</Text>
+			<Button
+				title="Go back"
+				onPress={() => {
+					props.navigation.popToTop();
+				}}
+			/>
 		</View>
 	);
 };
 
+export const screenOptions: NativeStackNavigationOptions = navigationData => {
+	const mealId = navigationData.route.params.mealId;
+
+	const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+	return {
+		headerTitle: selectedMeal?.title,
+		headerRight: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title="Favorite"
+					iconName="ios-star"
+					onPress={() => {
+						console.log("mark as favorite");
+					}}
+				/>
+			</HeaderButtons>
+		),
+	};
+};
+
 export default MealDetailScreen;
-
-interface Styles {}
-
-const styles = StyleSheet.create<Styles>({});
