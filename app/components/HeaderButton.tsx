01cbdccd1d274ed35c5281fc9b7c98fa0226b39e
Stavros <snikoletopoulos@hotmail.com>
2022-03-12 19:57:39 +0200

    Create button  for toggling favorites screens

---
+++ app/components/HeaderButton.tsx
@@ -0,0 +1,23 @@
+import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import Colors from "constants/colors";
+
+import { HeaderButton } from "react-navigation-header-buttons";
+
+type Props = {
+	title: string;
+};
+
+const CustomHeaderButton: React.FC<Props> = props => {
+	return (
+		<HeaderButton
+			{...props}
+			IconComponent={Ionicons}
+			iconSize={23}
+			color={Platform.OS === "android" ? "white" : Colors.primary}
+		/>
+	);
+};
+
+export default CustomHeaderButton;
