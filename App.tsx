d89d5150c3f0d969f9e9f8b13ea4ea066c4a4065
Stavros <snikoletopoulos@hotmail.com>
2022-03-12 19:50:27 +0200

    Remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import { enableScreens } from "react-native-screens";
