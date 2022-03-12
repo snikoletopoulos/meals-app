05a20e77898768edf6decacb2efb85ed120e5e64
Stavros <snikoletopoulos@hotmail.com>
2022-03-12 19:54:45 +0200

    Create favorites stack navigation

---
+++ app/navigation/FavoritesNavigator.tsx
@@ -0,0 +1,40 @@
+import { Platform, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Colors from "constants/colors";
+
+import FavoritesScreen from "screens/FavoritesScreen";
+import MealDetailScreen from "screens/MealDetailScreen";
+
+export type FavoritesParamList = {
+	FavoritesScreen: undefined;
+	MealScreen: {
+		mealId: string;
+	};
+};
+
+const Stack = createNativeStackNavigator<FavoritesParamList>();
+
+const FavoritesNavigator: React.FC = () => {
+	const { Navigator, Screen } = Stack;
+
+	return (
+		<View style={{ flex: 1 }} collapsable={false}>
+			<Navigator
+				initialRouteName="FavoritesScreen"
+				screenOptions={{
+					headerStyle: {
+						backgroundColor:
+							Platform.OS === "android" ? Colors.accent : "white",
+					},
+					headerTintColor: Platform.OS === "android" ? "white" : Colors.accent,
+				}}
+			>
+				<Screen name="FavoritesScreen" component={FavoritesScreen} />
+				<Screen name="MealScreen" component={MealDetailScreen} />
+			</Navigator>
+		</View>
+	);
+};
+
+export default FavoritesNavigator;
