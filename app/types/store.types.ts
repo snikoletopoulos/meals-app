969e18af91d17ef63cf28f2bec979df77748c9a8
Stavros <snikoletopoulos@hotmail.com>
2022-03-13 20:36:31 +0200

    Add types for actions thunk creators

--- app/types/store.types.ts
+++ app/types/store.types.ts
@@ -1,3 +1,11 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import store from "store/index";
 
-export type RootStore = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppAsyncThunk<T = void> = ThunkAction<
+	Promise<T>,
+	RootState,
+	unknown,
+	Action
+>;
+export type ActionCreatorThunk<T = void> = () => AppAsyncThunk<T>;
