53d1a06ee72e6a7a59c3cd0d80c237816eea2b95
Stavros <snikoletopoulos@hotmail.com>
2022-03-13 03:22:36 +0200

    Add drawer navigator in main app navigator

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,6 +1,13 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+	NavigationContainer,
+	NavigatorScreenParams,
+} from "@react-navigation/native";
+
+import Colors from "constants/colors";
 
 import TabNavigator, { TabParamList } from "navigation/TabNavigator";
+import FiltersNavigator, { FiltersParamList } from "./FiltersNavigator";
 
 declare global {
 	namespace ReactNavigation {
@@ -8,10 +15,42 @@ declare global {
 	}
 }
 
+type DrawerParamList = {
+	MealsFavs: NavigatorScreenParams<TabParamList>;
+	Filters: NavigatorScreenParams<FiltersParamList>;
+};
+
+const Drawer = createDrawerNavigator<DrawerParamList>();
+
 const AppNavigator: React.FC = () => {
+	const { Navigator, Screen } = Drawer;
+
 	return (
 		<NavigationContainer>
-			<TabNavigator />
+			<Navigator
+				screenOptions={{
+					headerShown: false,
+					drawerActiveTintColor: Colors.accent,
+					drawerLabelStyle: {
+						fontFamily: "open-sans-bold",
+					},
+				}}
+			>
+				<Screen
+					name="MealsFavs"
+					component={TabNavigator}
+					options={{
+						title: "Meals",
+					}}
+				/>
+				<Screen
+					name="Filters"
+					component={FiltersNavigator}
+					options={{
+						title: "Filters",
+					}}
+				/>
+			</Navigator>
 		</NavigationContainer>
 	);
 };
