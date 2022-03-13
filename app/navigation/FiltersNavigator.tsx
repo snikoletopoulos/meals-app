b62bac9e52c51eb6078c6f6e0f42df314e4f8425
Stavros <snikoletopoulos@hotmail.com>
2022-03-13 03:22:20 +0200

    Create filters native stack navigator

---
+++ app/navigation/FiltersNavigator.tsx
@@ -0,0 +1,32 @@
+import { Platform } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Colors from "constants/colors";
+
+import FiltersScreen from "screens/FiltersScreen";
+
+export type FiltersParamList = {
+	FiltersScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<FiltersParamList>();
+
+const FiltersNavigator: React.FC = () => {
+	const { Navigator, Screen } = Stack;
+
+	return (
+		<Navigator
+			initialRouteName="FiltersScreen"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
+				},
+				headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+			}}
+		>
+			<Screen name="FiltersScreen" component={FiltersScreen} />
+		</Navigator>
+	);
+};
+
+export default FiltersNavigator;
