088ff3e4f5ec3f230809ba03570634c546c8a6c4
Stavros <snikoletopoulos@hotmail.com>
2022-03-13 17:26:49 +0200

    Pass the props to the BaseText component

--- app/components/BaseText.tsx
+++ app/components/BaseText.tsx
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, TextProps, View } from "react-native";
+import { StyleSheet, Text, TextProps } from "react-native";
 import React from "react";
 
-const BaseText: React.FC = props => {
-	return <Text style={styles.text}>{props.children}</Text>;
+const BaseText: React.FC<TextProps> = props => {
+	return (
+		<Text {...props} style={[styles.text, props.style]}>
+			{props.children}
+		</Text>
+	);
 };
 
 export default BaseText;
