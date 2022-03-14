ef760791db77630fd9bc2b46133d415d2334d86a
Stavros <snikoletopoulos@hotmail.com>
2022-03-14 02:01:06 +0200

    Fix type issue

--- app/screens/FavoritesScreen.tsx
+++ app/screens/FavoritesScreen.tsx
@@ -5,12 +5,13 @@ import {
 import { useSelector } from "helpers/store";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { FavoritesParamList } from "navigation/FavoritesNavigator";
 
 import HeaderButton from "components/HeaderButton";
 import { TabParamList } from "navigation/TabNavigator";
 import MealList from "components/MealList";
 
-type Props = NativeStackScreenProps<TabParamList, "Favorites">;
+type Props = NativeStackScreenProps<FavoritesParamList, "FavoritesScreen">;
 
 const FavoritesScreen: React.FC<Props> = props => {
 	const favoriteMeals = useSelector(state => state.meals.favoriteMeals);
