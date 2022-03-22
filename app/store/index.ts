231189624dac040f7e76fd393d56bee73a97042d
Stavros <snikoletopoulos@hotmail.com>
2022-03-22 19:50:09 +0200

    Disable Redux midleware

--- app/store/index.ts
+++ app/store/index.ts
@@ -6,6 +6,10 @@ const store = configureStore({
 	reducer: {
 		meals: mealsReducer,
 	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
 });
 
 export default store;
